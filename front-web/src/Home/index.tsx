108f676dbe4b2ec48a8d0055ec91b8fb8108f0a8
Renan Rodrigues <renan_jacui@hotmail.com>
2021-01-08 20:42:28 -0300

    Add home page

---
+++ front-web/src/Home/index.tsx
@@ -0,0 +1,22 @@
+import './style.css'
+import { ReactComponent as MainImage} from './main.svg'
+
+function Home(){
+    return (
+        <div className="home-container">
+            <div className="home-content">
+                <div className="home-action">
+                    <h1 className="home-title">Faça seu pedido <br/> que entregamos pra você!!!</h1>
+                    <h3 className="home-subtitle">
+                        Escolha o seu pedido e em poucos minutos <br/>
+                        levaremos na usa porta
+                    </h3>
+                    <a href="orders" className="home-btn-order">FAZER PEDIDO</a>
+                </div>
+                <div className="home-image"> <MainImage /></div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
